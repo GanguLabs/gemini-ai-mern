0f71befa67d8ab9d4566b03d0ec23a008f208303
Anwesh Gangula <gangulaanwesh@gmail.com>
2024-03-31 15:19:12 +0530

    push code to git

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+const app = express();
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174']
+}
+const PORT = 8080;
+
+app.use(cors(corsOptions))
+app.use(express.json());
+
+app.post("/generate", async (req, res) => {
+    const {prompt} = req.body;
+    console.log({prompt})
+    try {
+        // For text-only input, use the gemini-pro model
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+        // const prompt = "Write a story about a magic backpack."
+        const result = await model.generateContent(prompt);
+
+        const response = await result.response;
+        const text = response.text();
+        // console.log(text);
+        res.send(text);
+    } catch (error) {
+        res.status(500).send("Failed to generate content")
+    }
+})
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('Server is ready'))
+}
+
+app.listen(PORT, console.log(`Server running at port: ${PORT}`))
---
+++ frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { useMutation } from '@tanstack/react-query'
+import './App.css'
+
+const makeRequestAPI = async (prompt) => {
+  const res = await axios.post('http://www.localhost:8080/generate', { prompt });
+
+  return res.data;
+}
+
+function App() {
+  const [prompt, setPrompt] = useState("")
+
+  const mutation = useMutation({
+    mutationFn: makeRequestAPI,
+    mutationKey: ['gemini-ai-request']
+  })
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    mutation.mutate(prompt);
+  }
+
+  console.log({ mutation })
+  return (
+    <div className='App'>
+      <header>Gemini AI Content Generator</header>
+      <p>Enter a prompt and let Gemini AI craft a unique content for you</p>
+
+      <form className='App-form' onSubmit={submitHandler}>
+        <label>Enter your prompt:</label>
+        <input
+          type="text"
+          name="prompt"
+          id="promptInput"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          placeholder='Write a content about...'
+          className='App-input'
+        />
+        <button className='App-button' type="submit">Generate button</button>
+
+        <section className='App-response'>
+          {mutation.isPending && <p>generating your conent</p>}
+          {mutation.isError && <p>{mutation.error.message}</p>}
+          {mutation.isSuccess && <p>{mutation.data}</p>}
+        </section>
+      </form>
+    </div>
+  )
+}
+
+export default App
